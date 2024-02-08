f7a10f0884fa6bca70c0bb50196c83ef72dfbc81
Folkert Niewijk <fniewijk@gmail.com>
2024-02-08 13:22:06 +0100

    day 2

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,9 +1,48 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
-export async function day2a(dataPath?: string) {
+const colorMaximums = {
+  red: 12,
+  green: 13,
+  blue: 14,
+}
+
+export async function day2a(dataPath?: string): Promise<number> {
   const data = await readData(dataPath);
-  return 0;
+  const gameResults = data.map((line) => {
+    const [game, gameResult] = line.split(': ');
+    const gameNumber = game.split(' ')[1];
+
+    const results = gameResult.split('; ');
+    const incorrectGame = results.some((result) => {
+      const roundResults = result.split(', ');
+      const colorSums = roundResults.reduce((prev, cur) => {
+        const [score, color] = cur.split(' ');
+        return {
+          ...prev,
+          [color]: prev[color] + parseInt(score),
+        }
+      }, { red: 0, green: 0, blue: 0});
+
+
+      const incorrectRound = Object.entries(colorSums).some((colorSum) => {
+        const [color, score] = colorSum;
+        if (score > colorMaximums[color]) {
+          console.log(`Game ${gameNumber} is disqualified, because of ${color} ${score} is higher than ${colorMaximums[color]}!`);
+          return true;
+        }
+        return false;
+      });
+      
+      return incorrectRound;
+    });
+
+    if(!incorrectGame) {
+      return parseInt(gameNumber);
+    }
+    return 0;
+  });
+  return gameResults.reduce((acc, curr) => acc + curr, 0);
 }
 
 const answer = await day2a();
--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -1,9 +1,38 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
-export async function day2b(dataPath?: string) {
+const colorMaximums = {
+  red: 12,
+  green: 13,
+  blue: 14,
+}
+
+export async function day2b(dataPath?: string): Promise<number> {
   const data = await readData(dataPath);
-  return 0;
+  const gameResults = data.map((line) => {
+    const [game, gameResult] = line.split(': ');
+    const results = gameResult.split('; ');
+    const colorSum = results.reduce((prev, cur) => {
+      const roundResults = cur.split(', ');
+      const colorSums = roundResults.reduce((prev, roundResult) => {
+        const [score, color] = roundResult.split(' ');
+        return {
+          ...prev,
+          [color]: Math.max(prev[color], parseInt(score)),
+        }
+      }, { red: 0, green: 0, blue: 0 });
+
+      return {
+        red: Math.max(prev.red, colorSums.red),
+        green: Math.max(prev.green, colorSums.green),
+        blue: Math.max(prev.blue, colorSums.blue),
+      }
+    }, { red: 0, green: 0, blue: 0 });
+
+    console.log(colorSum, colorSum.red * colorSum.green * colorSum.blue);
+    return colorSum.red * colorSum.green * colorSum.blue;
+  });
+  return gameResults.reduce((acc, curr) => acc + curr, 0);
 }
 
 const answer = await day2b();
