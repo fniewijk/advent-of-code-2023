ce93f50c5372cda3908a95386a6531097f84ad7f
Folkert Niewijk <fniewijk@gmail.com>
2024-02-08 17:03:48 +0100

    4a done

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -3,7 +3,24 @@ import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const cardResults = data.reduce((prev, line) => {
+    const [game, gameResult] = line.split(': ');
+    const gameNumber = game.split(' ')[1];
+
+    const numbers = gameResult.split(' | ');
+
+    const winningNumbers = numbers[0].split(' ').filter((number) => number !== '');
+    const myNumbers = numbers[1].split(' ').filter((number) => number !== '');;
+
+    const myWinningNumbers = myNumbers.filter((number) => winningNumbers.includes(number));
+
+    console.log(prev);
+    console.log(gameNumber, myWinningNumbers);
+    return prev + (myWinningNumbers.length > 0 ? Math.pow(2, myWinningNumbers.length - 1) : 0);
+  },0);
+    
+  return cardResults;
 }
 
 const answer = await day4a();
