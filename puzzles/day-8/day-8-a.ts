170f9b79db7987f9f5cf5cc0bbde9e24771eed06
Folkert Niewijk <fniewijk@gmail.com>
2024-02-26 13:31:00 +0100

    day 8

--- puzzles/day-8/day-8-a.ts
+++ puzzles/day-8/day-8-a.ts
@@ -3,7 +3,46 @@ import chalk from 'chalk';
 
 export async function day8a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const instructions = data[0].split('');
+  const maps = data.slice(2).reduce((prev, map) => {
+
+    // console.log('map', map);
+    const [key, rest] = map.split(' = ');
+    // console.log('key', key, 'rest', rest);
+    // turn '(BBB, CCC)' into { left: BBB, right: CCC }
+    const [left, right] = rest.slice(1, rest.length - 1).split(', ');
+    return { ...prev, [key]: { left, right } };
+  }, {});
+  
+  let currentMap = maps['AAA'];
+  let instructionIndex = 0;
+  // console.log('lastKey', currentMap, maps['lastKey'])
+  while (true ) {
+
+    const instruction = instructions[instructionIndex % instructions.length];
+    instructionIndex++;
+    // console.log(instructionIndex);
+
+    const { left, right } = currentMap;
+    console.log('instruction', instruction, 'left', left, 'right', right);
+    if (instruction === 'R') {
+      currentMap = maps[right];
+      if (right == 'ZZZ') {
+        break;
+      }
+      console.log('R', right);
+    }
+    if (instruction === 'L') {
+      currentMap = maps[left];
+      if (left == 'ZZZ') {
+        break;
+      }
+      console.log('L', left);
+    }
+
+  }
+  return instructionIndex;
 }
 
 const answer = await day8a();
--- puzzles/day-8/day-8-b.ts
+++ puzzles/day-8/day-8-b.ts
@@ -1,9 +1,79 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+interface MapObject {
+  [key: string]: Map;
+}
+
+interface Map {
+  L: string;
+  R: string;
+}
+
+const looper = (currentMap: Map, maps: MapObject, instructions: string[]) => {
+  let instructionIndex = 0;
+  while (true) {
+    const instruction = instructions[instructionIndex % instructions.length];
+    instructionIndex++;
+
+    const { L, R } = currentMap;
+
+    const key = instruction === 'R' ? R : L;
+
+    currentMap = maps[key];
+
+    if(key.endsWith('Z')) break;
+  }
+  return instructionIndex;
+};
+
+const getCommonDivider = (a: number, b: number) => {
+  return !b ? a : getCommonDivider(b, a % b);
+}
+
+const getLeastCommonMultiple = (a: number, b: number) => {
+  return (a * b) / getCommonDivider(a, b);
+}
+
+
 export async function day8b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  const instructions = data[0].split('');
+
+  let activeMaps: MapObject = {};
+
+  const maps: MapObject = data.slice(2).reduce((prev, map) => {
+
+    // console.log('map', map);
+    const [key, rest] = map.split(' = ');
+    // console.log('key', key, 'rest', rest);
+    // turn '(BBB, CCC)' into { left: BBB, right: CCC }
+    const [L, R] = rest.slice(1, rest.length - 1).split(', ');
+
+    if(key.endsWith('A')) {
+      activeMaps = { ...activeMaps, [key]: { L, R } };
+    }
+
+    return { ...prev, [key]: { L, R } };
+  }, {});
+  let instructionIndex = 0;
+  // console.log('lastKey', currentMap, maps['lastKey']);
+
+  console.log('activeMaps', activeMaps);
+
+  const instructionCounts = Object.keys(activeMaps).map((key) => {
+    return looper(activeMaps[key], maps, instructions);
+  });
+
+  // find lowest common multiple in instructionCounts
+  console.log(instructionCounts);
+
+  const lcmValue = instructionCounts.reduce((prev, count) => {
+    return getLeastCommonMultiple(prev, count);
+  });
+
+  return lcmValue;
 }
 
 const answer = await day8b();
